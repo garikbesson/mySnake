8c455b8733a8a9f43e916de1e31aaa7da8830cf1
garikbesson <garikbesson@gmail.com>
2015-09-27 22:53:50 +0600

    Создан класс "змейка"

--- mySnake/mySnake/Program.cs
+++ mySnake/mySnake/Program.cs
@@ -25,8 +25,8 @@ namespace mySnake
 
             //Отрисовка точек
             Point p = new Point(4, 5, '*');
-            p.Draw();
-
+            Snake snake = new Snake(p, 4, Direction.RIGHT);
+            snake.Drow();
         }
     }
 }
